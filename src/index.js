810cc4c1b16f7487bf1d0c6ae8fbd2d906fc942e
Lena-Yasynetska <123744975+Lena-Yasynetska@users.noreply.github.com>
2023-11-11 14:12:16 +0100

    10/5

--- src/index.js
+++ src/index.js
@@ -1,61 +1,84 @@
-import { fetchBreeds, fetchCatByBreed } from "./cat-api";
-import './styles.css';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SlimSelect from 'slim-select'
-import 'slim-select/dist/slimselect.css';
+import { fetchBreeds } from "./helpers/cat-api";
+import { fetchCatByBreed } from "./helpers/cat-api";
+import { Report } from 'notiflix/build/notiflix-report-aio';
+// import SlimSelect from 'slim-select'
 
-const ref = {
-    selector: document.querySelector('.breed-select'),
-    divCatInfo: document.querySelector('.cat-info'),
-    loader: document.querySelector('.loader'),
-    error: document.querySelector('.error'),
-};
-const { selector, divCatInfo, loader, error } = ref;
 
-loader.classList.replace('loader', 'is-hidden');
-error.classList.add('is-hidden');
-divCatInfo.classList.add('is-hidden');
+// Globale constanten
+const selectPlaceholder = `<option class="js-selectOption js-placeholder-select" value="choose">Select the cat</option>`;
 
-let arrBreedsId = [];
+// Query selectors
+const select = document.querySelector(".breed-select");
+const catInfoCard = document.querySelector(".cat-info");
+const loaderMessage = document.querySelector(".loader");
+const errorMessage = document.querySelector(".error");
+
+select.insertAdjacentHTML("afterbegin", selectPlaceholder);
+
+
+loaderMessage.hidden = false;
+select.hidden = true;
+
+function markupSelect(arr) {
+   return arr.map(({name, id}) => {
+    return `<option class="js-selectOption" value="${id}">${name}</option>`
+   }).join("");
+}
+
 fetchBreeds()
 .then(data => {
-    data.forEach(element => {
-        arrBreedsId.push({text: element.name, value: element.id});
-    });
-    new SlimSelect({
-        select: selector,
-        data: arrBreedsId
-    });
-    })
-.catch(onFetchError);
+    select.insertAdjacentHTML("beforeend", markupSelect(data));
+    // new SlimSelect({
+    //     select: '#selectElement'
+    //   })
+    loaderMessage.hidden = true;
+    select.hidden = false;
+})
+.catch(err => console.log(err))
 
-selector.addEventListener('change', onSelectBreed);
+// Event listener for select
+select.addEventListener("change", onChangeSelect);
 
-function onSelectBreed(event) {
-    loader.classList.replace('is-hidden', 'loader');
-    selector.classList.add('is-hidden');
-    divCatInfo.classList.add('is-hidden');
+function onChangeSelect() {
+    catInfoCard.classList.add("cat-card");
+    errorMessage.hidden = true;
+    loaderMessage.hidden = false;
+    select.hidden = true;
+    // console.log(select.value);
+   fetchCatByBreed(select.value)
+   .then(data => {
+    // console.log(data[0]);
+    const img = data[0].url;
+    const name = data[0].breeds[0].name;
+    const description = data[0].breeds[0].description;
+    const temperament = data[0].breeds[0].temperament;
+   
+catInfoCard.innerHTML = createCatCard(img, name, description, temperament);
+loaderMessage.hidden = true;
+select.hidden = false;
+catInfoCard.classList.remove("cat-card");
+})
+.catch(err => {
+    Report.failure(
+        'Oops! Something went wrong!',
+        'Please try again later',
+        'Okay',
+        );
+    // errorMessage.hidden = false;
+    loaderMessage.hidden = true;
+select.hidden = false;
+    console.log(err)
+    catInfoCard.classList.add("cat-card");
+})
+}
 
-    const breedId = event.currentTarget.value;
-    fetchCatByBreed(breedId)
-    .then(data => {
-        loader.classList.replace('loader', 'is-hidden');
-        selector.classList.remove('is-hidden');
-        const { url, breeds } = data[0];
-        divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`
-        divCatInfo.classList.remove('is-hidden');
-    })
-    .catch(onFetchError);
-};
+    function createCatCard(img, name, description, temperament){
 
-function onFetchError(error) {
-    selector.classList.remove('is-hidden');
-    loader.classList.replace('loader', 'is-hidden');
-
-    Notify.failure('Oops! Something went wrong! Try reloading the page or select another cat breed!', {
-        position: 'center-center',
-        timeout: 5000,
-        width: '400px',
-        fontSize: '24px'
-    });
-};
+    return `<img class="js-cat-photo" src="${img}" alt="${name}" width="300">
+    <div class="js-cat-description">
+      <h2>${name}</h2>
+      <p>${description}</p>
+      <h3>Temperament:</h3>
+      <p>${temperament}</p>
+    </div>`;
+}
